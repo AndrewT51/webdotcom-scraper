6c39a274d7a4017803be8949df1af470b52f667f
Andrew Trigg <andrewt51@hotmail.com>
2018-02-01 11:29:30 +0000

    Use fs to read the yaml file from the structure-file folder and convert it into js object

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const [ structureFile, uri ] = process.argv.slice(2)
+const [ structureFile, uri ] = process.argv.slice(2);
+const yaml = require('js-yaml');
+const fs = require('fs');
+const path = require('path')
 
+try {
+  const structureFileLocation = path.join(__dirname, `./structure-files/${structureFile}.yml`);
+  const fileContent = fs.readFileSync(structureFileLocation)
+  const doc = yaml.safeLoad(fileContent)
+  console.log(doc);
+} catch (e) {
+  console.log(e);
+}
