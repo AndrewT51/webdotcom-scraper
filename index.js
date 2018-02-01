5c82a7b52ca6081f0dfdf55af517fae4f3d10905
Andrew Trigg <andrewt51@hotmail.com>
2018-02-01 17:29:30 +0000

    Add catch statement to deal with any error. Add a last link to the promise chain to ensure the process is closed

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const colors = {
+  SUCCESS: 'green',
+  ERROR: 'red'
+}
 
 let docElements;
 let resultJson = {};
@@ -22,14 +26,25 @@ try {
 }
 
 const scraper = async() => {
-  const response = await axios.get(`http://${uri}`);
+
+  const response = await axios.get(`http://${ur}`);
   const $ = cheerio.load(response.data);
   for (key in docElements){
     if (docElements.hasOwnProperty(key)){
       resultJson[key] = $(docElements[key]).text().replace(/\n+| +/g,' ');
     }
   }
-  console.log(formatOutput('blue', resultJson))
+  return resultJson
 }
 
 scraper()
+.then( result => {
+  console.log(formatOutput(colors.SUCCESS, result))
+})
+.catch( err => {
+  console.error(formatOutput(colors.ERROR, err));
+})
+.then( () => {
+  console.log('Exiting process!');
+  process.exit()
+})
